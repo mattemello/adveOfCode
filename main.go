a13d8c33bc63cfd9f969eb256c96660b074c0573
matteo <matteodragonfire2312@gmail.com>
2024-10-15 15:16:54 +0200

    try to go futer

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ func main() {
 	//fmt.Printf("value trovato: \n\t %d\n", num)
 	//fmt.Println(GameMinNumber())
 	//fmt.Print(takeFromTheFile())
-	fmt.Println(pipeNumber())
+	fmt.Println(PipeNumber2())
 }
--- pipeWinning.go
+++ pipeWinning.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 )
@@ -32,6 +33,116 @@ func grabNumber(text string) map[int]bool {
 	}
 
 	return numbers
+}
+
+func PipeNumber2() int {
+
+	file, err := os.Open("./test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var n = make(map[int]int)
+
+	i := 0
+	for scanner.Scan() {
+		i++
+		text := scanner.Text()
+
+		position := 0
+		for text[position] != ':' {
+			position++
+		}
+
+		text = text[position:]
+
+		position = 0
+		for text[position] != '|' {
+			position++
+		}
+
+		winningNumber := grabNumber(text[:position])
+		myNumber := grabNumber(text[position:])
+
+		timeIn := 0
+
+		for key, _ := range myNumber {
+			_, isIn := winningNumber[key]
+
+			if isIn {
+				timeIn += 1
+			}
+		}
+
+		n[i] = 0
+		if timeIn > 0 {
+			n[i] = timeIn
+		}
+
+	}
+
+	return sum(n)
+}
+
+/*
+	tutte le istanze sono 1
+
+	  1     2     3     4     5     6
+	[ 1 ] [ 1 ] [ 1 ] [ 1 ] [ 1 ] [ 1 ]
+	  4
+	[ 1 ] [ 1+1 ] [ 1+1 ] [ 1+1 ] [ 1+1 ] [ 1 ]
+			2
+	[ 1 ] [ 1+1 ] [ 1+1+1+1 ] [ 1+1+1+1 ] [ 1+1 ] [ 1 ]
+				  2
+	[ 1 ] [ 1+1 ] [ 1+1+1+1 ] [ 1+1+1+1 ] [ 1 ] [ 1 ]
+
+	[ 1 ] [ 1 ] [ 1 ] [ 1 ] [ 1 ] [ 1 ]
+*/
+
+/*
+	map[int]int
+	[1]->4
+	[2]->2    ->2
+	[3]->2    ->4
+	[4]->1    ->8
+	[5]->0    ->
+	[6]->0
+*/
+
+func sum(n map[int]int) int {
+
+	sum := 0
+	dim := len(n)
+	p := make([]int, dim)
+	for i, _ := range p {
+		p[i] = 0
+	}
+
+	for i, m := range n {
+		fmt.Println("[ ", i, " ] -> ", m)
+		if p[i] > 0 {
+
+			for c := 0; c < p[i]; c++ {
+				for j := 0; j < m; j++ {
+					p[i+m-1] += 1
+				}
+			}
+		}
+		for m >= 0 {
+			p[i+m-1] += 1
+			m--
+		}
+	}
+
+	for _, i := range p {
+		sum += p[i]
+	}
+
+	return sum
+
 }
 
 func pipeNumber() int {
